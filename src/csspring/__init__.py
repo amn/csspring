06175cf827443a0576027e8fa99b9878450bd56d
Armen Michaeli <armen@armen.michaeli.name>
2024-09-30 21:40:09 +0200

    Fix a critical issue with the `selector_list` attribute of `QualifedRule`
    The "prelude" component of a qualified rule is a list of component values, not just tokens. Parsing it as a selector list, per the `parse_selector_list` API in the `selectors` module, demands a list of _tokens_ (currently), so a `tokens` call is necessary to "flatten" the list of component values. This adds the call, before normalization.

--- src/csspring/__init__.py
+++ src/csspring/__init__.py
@@ -5,10 +5,10 @@ def _enable_selector_parsing():
 
     This adds on-demand parsing of the prelude part of every qualified rule (see the `prelude` attribute on `QualifiedRule`), when the `selector_list` property is accessed on the latter. Because parsing is only done when the property added with this procedure, is accessed, `csspring.syntax` remains compliant with its respective specification.
     """
-    from .syntax.parsing import normalize_input, QualifiedRule
+    from .syntax.parsing import normalize_input, QualifiedRule, tokens
     from .selectors import parse_selector_list
     def qualified_rule_selector_list(self):
-        return parse_selector_list(normalize_input(self.prelude))
+        return parse_selector_list(normalize_input(tokens(self.prelude)))
     setattr(QualifiedRule, "selector_list", property(qualified_rule_selector_list))
 
 _enable_selector_parsing()
