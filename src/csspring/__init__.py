889e7ba5bbdcf0b8b55ef1fd965be5d50a0df8ae
Armen Michaeli <armen@armen.michaeli.name>
2024-09-30 21:50:17 +0200

    Add type hints to the init. module of the package
    The module in question was missing type hinting entirely, which only contributed to the issue the parent commit fixes. This adds type hinting, which also helps validate the initialization behaviour.

--- src/csspring/__init__.py
+++ src/csspring/__init__.py
@@ -1,13 +1,13 @@
 """Parsing of CSS text aligned with [CSS] specification(s)."""
 
-def _enable_selector_parsing():
+def _enable_selector_parsing() -> None:
     """Augment parsing procedures to enable parsing of CSS selectors.
 
     This adds on-demand parsing of the prelude part of every qualified rule (see the `prelude` attribute on `QualifiedRule`), when the `selector_list` property is accessed on the latter. Because parsing is only done when the property added with this procedure, is accessed, `csspring.syntax` remains compliant with its respective specification.
     """
-    from .syntax.parsing import normalize_input, QualifiedRule, tokens
+    from .syntax.parsing import normalize_input, Product, QualifiedRule, tokens
     from .selectors import parse_selector_list
-    def qualified_rule_selector_list(self):
+    def qualified_rule_selector_list(self: QualifiedRule) -> Product | None:
         return parse_selector_list(normalize_input(tokens(self.prelude)))
     setattr(QualifiedRule, "selector_list", property(qualified_rule_selector_list))
 
